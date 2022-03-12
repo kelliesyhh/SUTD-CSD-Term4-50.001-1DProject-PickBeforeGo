ecc70f636aefce977132948e11fcc6341b7d5629
kelliesyhh <46555086+kelliesyhh@users.noreply.github.com>
2022-03-12 21:48:53 +0800

    Add no stock product card, add product screen

---
+++ android/app/src/main/java/com/example/local_1d/ProductScreen.java
@@ -0,0 +1,27 @@
+package com.example.local_1d;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class ProductScreen extends AppCompatActivity {
+
+    FragmentTransaction transaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_screen);
+        Fragment availability = new Availability();
+        Fragment productCard = new ProductCard();
+        FragmentManager manager = getSupportFragmentManager();
+        transaction = manager.beginTransaction();
+        transaction.add(R.id.fragment_productCard, productCard, "Product_Card");
+        transaction.add(R.id.fragment_availability, availability, "Availability");
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
