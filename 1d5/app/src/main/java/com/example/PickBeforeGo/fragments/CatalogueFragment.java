973a91ff2454dfa3856ae358b6fd958df00a29da
Fruittips <yida_mah@mymail.sutd.edu.sg>
2022-03-24 23:53:23 +0800

    Added helper function to call db and pass data b/w activity n fragments

--- 1d5/app/src/main/java/com/example/PickBeforeGo/fragments/CatalogueFragment.java
+++ 1d5/app/src/main/java/com/example/PickBeforeGo/fragments/CatalogueFragment.java
@@ -9,15 +9,17 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.PickBeforeGo.MainActivity;
 import com.example.PickBeforeGo.R;
 import com.example.PickBeforeGo.adapters.ProductRVAdapter;
 import com.example.PickBeforeGo.components.Product;
+import com.example.PickBeforeGo.helper.GetProduct_helper;
 
 import java.util.ArrayList;
 
 public class CatalogueFragment extends Fragment {
     private final int position;
-    private ArrayList<Product> productArrayList;
+    private ArrayList<Product> productArrayList = new ArrayList<Product>();
 
     public CatalogueFragment(int position){
         this.position = position;
@@ -33,26 +35,19 @@ public class CatalogueFragment extends Fragment {
 
         RecyclerView productRV = rootView.findViewById(R.id.product_rv);
 
+        //TODO to get All products and can do sorting uh
+        MainActivity main_activity = (MainActivity) getActivity();
+
+
         //All Tab
         if (position == 0){
-
-            productArrayList.add(new Product.ProductBuilder().setProductName("White Bread").setDescription("Gardenia").setWeight("500g").build());
-            productArrayList.add(new Product.ProductBuilder().setProductName("White Bread").setDescription("Gardenia").setWeight("500g").build());
-            productArrayList.add(new Product.ProductBuilder().setProductName("White Bread").setDescription("Gardenia").setWeight("500g").build());
-            productArrayList.add(new Product.ProductBuilder().setProductName("White Bread").setDescription("Gardenia").setWeight("500g").build());
-            productArrayList.add(new Product.ProductBuilder().setProductName("White Bread").setDescription("Gardenia").setWeight("500g").build());
+//            assert main_activity != null;
+            productArrayList = main_activity.getAllProducts();
         }
 
         // Favourites Tab
         else if (position == 1){
-            //get data from database and populate catalogue here
-            productArrayList.add(new Product.ProductBuilder().setProductName("White Bread").setDescription("Gardenia").setWeight("500g").build());
-            productArrayList.add(new Product.ProductBuilder().setProductName("White Bread").setDescription("Gardenia").setWeight("500g").build());
-            productArrayList.add(new Product.ProductBuilder().setProductName("White Bread").setDescription("Gardenia").setWeight("500g").build());
-            productArrayList.add(new Product.ProductBuilder().setProductName("White Bread").setDescription("Gardenia").setWeight("500g").build());
-            productArrayList.add(new Product.ProductBuilder().setProductName("White Bread").setDescription("Gardenia").setWeight("500g").build());
-
-
+            productArrayList = main_activity.getFavouriteProducts();
         }
 
         ProductRVAdapter productRVAdapter = new ProductRVAdapter(getActivity(), productArrayList);
